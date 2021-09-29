156de145b3b5a7e226882e886dc5e19c68cc90d9
Aaditya Jaiswal <78849620+AADI-1331@users.noreply.github.com>
2021-09-29 11:18:49 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+#Hints:
+#In case of input data being supplied to the question, it should be assumed to be a console input.
+
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+x=int(raw_input())
+print fact(x)
