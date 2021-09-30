92b5456ae6789fcfe41b237308ba91b17c05f820
Aditi270 <90084243+Aditi270@users.noreply.github.com>
2021-09-30 21:18:34 +0530

    Update factorial.py
    Need a review and updated the code.

--- factorial.py
+++ factorial.py
@@ -2,10 +2,15 @@
 #In case of input data being supplied to the question, it should be assumed to be a console input.
 
 def fact(n):
+    
     if n == 0:
+        
         return 1
-    return n * fact(n - 1)
+   
+return n * fact(n - 1)
+
 
 n=int(console_input())
+
 
 print fact(n) 
